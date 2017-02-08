3e71ad723fb844cf93658e2b584332b39d7f6484
Kevin McCormack <harlemsquirrel@gmail.com>
2017-02-07 22:35:45 -0500

    Add preferences
    - Allow settings emoji font size

--- emoji_menu.js
+++ emoji_menu.js
@@ -2,16 +2,18 @@ const Clutter = imports.gi.Clutter;
 const Lang = imports.lang;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
+const Shell = imports.gi.Shell;
 const St = imports.gi.St;
 
 const Me = imports.misc.extensionUtils.getCurrentExtension();
+const Convenience = Me.imports.convenience;
 const Emojis = Me.imports.emojis;
 const EmojiButton = Me.imports.emoji_button.EmojiButton;
 
 
 const EmojiMenu = new Lang.Class({
-  Name: 'EmojiMenu',   // Class Name
-  Extends: PanelMenu.Button,  // Parent Class
+  Name: 'EmojiMenu',
+  Extends: PanelMenu.Button,
 
   _init: function() {
     this.parent(0, 'EmojiMenu', false);
@@ -35,6 +37,9 @@ const EmojiMenu = new Lang.Class({
     this.addEmojiSet('Objects', Emojis.OBJECTS, this)
     this.addEmojiSet('Symbols', Emojis.SYMBOLS, this)
     this.addEmojiSet('Flags', Emojis.FLAGS, this)
+
+    let prefsMenuItem = this.prefsMenuItem()
+    this.menu.addMenuItem(prefsMenuItem);
   },
 
   addEmojiSet: function(title, emojiSet, menuBase) {
@@ -56,9 +61,25 @@ const EmojiMenu = new Lang.Class({
       container.add_child(button, {hover: true});
     }
 
-
     menuBase.menu.addMenuItem(newMenuSet);
-    newMenuSet.menu.box.style_class = 'EmojisItemStyle';
+    let fontSize = Convenience.getSettings().get_int('font-size');
+    newMenuSet.menu.box.style_class = 'emoji-size-' + fontSize;
+  },
+
+  prefsMenuItem: function() {
+    let _appSys = Shell.AppSystem.get_default();
+    let _prefs = _appSys.lookup_app('gnome-shell-extension-prefs.desktop');
+    item = new PopupMenu.PopupMenuItem(_("Preferences..."));
+    item.connect('activate', function () {
+      if (_prefs.get_state() == _prefs.SHELL_APP_STATE_RUNNING){
+        _prefs.activate();
+      } else {
+        let info = _prefs.get_app_info();
+        let timestamp = global.display.get_current_time_roundtrip();
+        info.launch_uris([Me.metadata.uuid], global.create_app_launch_context(timestamp, -1));
+      }
+    });
+    return item;
   },
 
   destroy: function() {
---
+++ prefs.js
@@ -0,0 +1,40 @@
+const GObject = imports.gi.GObject;
+const Gtk = imports.gi.Gtk;
+const Lang = imports.lang;
+
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+const Convenience = Me.imports.convenience;
+
+function init() {
+}
+
+const EmojiBucketsPrefsWidget = new GObject.Class({
+  Name: 'EmojiBuckets.Prefs.Widget',
+  GTypeName: 'EmojiBucketsPrefsWidget',
+  Extends: Gtk.Grid,
+
+  _init: function(params) {
+    this.parent(params);
+    this.margin = this.row_spacing = this.column_spacing = 20;
+
+    this._settings = Convenience.getSettings();
+
+    this.attach(new Gtk.Label({ label: "Font size (requires restart)" }), 0, 0, 1, 1);
+    let fontSize = Gtk.Scale.new_with_range(Gtk.Orientation.HORIZONTAL, 1, 3, 1);
+    fontSize.set_value(this._settings.get_int('font-size'));
+    fontSize.set_digits(0);
+    fontSize.set_hexpand(true);
+    fontSize.connect('value-changed', Lang.bind(this, this._onFontSizeChanged));
+    this.attach(fontSize, 1, 0, 1, 1);
+  },
+
+  _onFontSizeChanged: function (fontSize) {
+    this._settings.set_int('font-size', fontSize.get_value());
+  },
+});
+
+function buildPrefsWidget() {
+  let widget = new EmojiBucketsPrefsWidget();
+  widget.show_all();
+  return widget;
+}
